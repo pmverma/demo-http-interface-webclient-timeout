87b1ba7e7f5c369fa1b6fff1903b1703077a87ab
pmohan <pmohan@ascendcorp.com>
2023-03-31 00:14:09 +0700

    Initial commit

---
+++ src/main/java/com/example/demohttpinterfacewebclienttimeout/DemoHttpInterfaceWebclientTimeoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohttpinterfacewebclienttimeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHttpInterfaceWebclientTimeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHttpInterfaceWebclientTimeoutApplication.class, args);
+	}
+
+}
